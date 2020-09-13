bbe6524a4822c1cfa39d945bd4549607a5b25a05
RemyMartin <RemyMartin ngohoanglam65@gmail.com>
2020-09-13 16:35:48 +0700

    add crawl external links

--- wiki_crawler/services/guitar_plus_crawl.py
+++ wiki_crawler/services/guitar_plus_crawl.py
@@ -7,7 +7,8 @@ import csv
 
 class GuitarPlusCrawler:
     def __init__(self):
-        self.link_set = set()
+        self.internal_link_set = set()
+        self.external_link_set = set()
 
     def generate_headers(self):
         """return human headers"""
@@ -33,47 +34,11 @@ class GuitarPlusCrawler:
             print('[Error] Error sending request to {}'.format(target_url))
         return None
 
-    def get_links(self, target_url):
-        """recursion to traverse all internal link from starter url"""
-        soup = self.get_soup(target_url=target_url)
-
-        if soup is None:
-            return None
-
-        # get html from tag <body>
-        body = soup.find(name='body')
-        # get html from tag <a>
-        href_regex = re.compile('(^https://guitarplus.com.*)'  # starts with 'https://guitarplus.com'
-                                '|'  # or
-                                '(^/.*)',  # start with '/'
-                                re.IGNORECASE | re.VERBOSE)
-        if body:
-            a_tags = body.find_all(name='a', attrs={'href': href_regex})
-            if a_tags:
-                for a_tag in a_tags:
-                    link = a_tag.attrs['href']
-                    if link.startswith('/'):
-                        link = self.convert_relative_path(current_url=target_url, path=link)
-                    if link not in self.link_set:
-                        self.link_set.add(link)
-                        print(link)
-                        self.get_links(link)
-
-    def get_link(self, url):
-        """recursion to traverse all internal link from starter url"""
-        """extract all internal links in web page, return set of all links"""
-        all_link_set = set()
-
-        soup = self.get_soup(target_url=url)
-        if soup is None:
-            return None
-        link_set = self.get_link(soup)
-
-        return sorted(all_link_set)
-
     def convert_relative_path(self, current_url, path):
         """convert web page relative path to absolute path"""
-        return self.get_domain(current_url) + path
+        if path.startswith('/'):
+            return self.get_domain(current_url) + path
+        return path
 
     def get_domain(self, url):
         """extract domain from url"""
@@ -83,9 +48,57 @@ class GuitarPlusCrawler:
     def export_csv(self, store_path, contents):
         """export data to store as csv in local"""
 
+    def get_hrefs_by_regex(self, soup, regex):
+        """find all hrefs in <a> tag follow regex"""
+        if soup is None:
+            return None
+        # get html from tag <body>
+        body = soup.find(name='body')
+        if body:
+            # get all <a> tag contain attr href with regex
+            a_tags = body.find_all(name='a', attrs={'href': regex})
+            return a_tags
+        return None
+
+    def get_internal_links(self, target_url):
+        """recursion to traverse all internal link from starter url"""
+        self.internal_link_set.add(target_url)
+        soup = self.get_soup(target_url=target_url)
+
+        # get html from tag <a>
+        href_regex = re.compile('(^https://guitarplus.com.*)'  # starts with 'https://guitarplus.com'
+                                '|'  # or
+                                '(^/.*)',  # start with '/'
+                                re.IGNORECASE | re.VERBOSE)
+
+        for a_tag in self.get_hrefs_by_regex(soup=soup, regex=href_regex):
+            link = a_tag.attrs['href']
+            link = self.convert_relative_path(current_url=target_url, path=link)
+            if link not in self.internal_link_set:
+                self.internal_link_set.add(link)
+                print(link)
+                self.get_internal_links(link)
+
+    def get_external_links(self, target_url):
+        """recursion to traverse all external link from starter url"""
+        soup = self.get_soup(target_url=target_url)
+
+        if soup is None:
+            return None
+
+        # get html from tag <a>
+        href_regex = re.compile('(^https|http|www)'  # starts with https|http|www
+                                '((?!guitarplus.com)'  # does not follow with 'guitarplus.com'
+                                '.)*$',
+                                re.IGNORECASE | re.VERBOSE)
+        for a_tag in self.get_hrefs_by_regex(soup=soup, regex=href_regex):
+            link = a_tag.attrs['href']
+            self.external_link_set.add(link)
+            print(link)
+
 
 if __name__ == '__main__':
     # execute script only
     start_link = 'https://guitarplus.com/'
     guitarPlusCrawler = GuitarPlusCrawler()
-    guitarPlusCrawler.get_links(target_url=start_link)
+    guitarPlusCrawler.get_internal_links(target_url=start_link)
